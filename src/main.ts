fc6250988dbf3af80e3d76e11855b1c5474587fc
Sebastian Rojas <sebosinaga@gmail.com>
2025-04-29 07:46:23 -0400

    modified the email sending logic and harcoded a test email address

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { connectRedis } from './redis/redis.client';
 import { registerInfoRequestListener } from './listeners/solicitud-info.listener';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main')
   const app = await NestFactory.create(AppModule);
 
   await connectRedis();
@@ -21,5 +23,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 5003);
+  logger.log(`Server is running on port ${process.env.PORT}`);
 }
 bootstrap();
